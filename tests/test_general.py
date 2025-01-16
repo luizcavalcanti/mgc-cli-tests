07d2c8d4cfd1db62596c59a1d9a4adaaf9ac60ad
Luiz Cavalcanti <luiz.cavalcanti@luizalabs.com>
2025-01-16 10:19:51 -0300

    fix: version test

--- tests/test_general.py
+++ tests/test_general.py
@@ -3,4 +3,4 @@ from utils import run_cli
 def test_general_version():
     exit_code, stdout, _, _ = run_cli(["--version"])
     assert exit_code == 0
-    assert "mgc version v0.31.1" in stdout
+    assert "mgc version v" in stdout
