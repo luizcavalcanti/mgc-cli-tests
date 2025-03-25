9f6a67d743a0b37e64863d3015fe918a1dff4357
Luiz Cavalcanti <luiz.cavalcanti@luizalabs.com>
2025-03-25 18:43:57 -0300

    feat: add network nat gateway tests

--- tests/test_network.py
+++ tests/test_network.py
@@ -15,10 +15,10 @@ network_test_context = {}
 
 
 def test_network_vpcs_create():
-    exit_code, stdout, stderr, jsonout = run_cli(
+    exit_code, _, stderr, jsonout = run_cli(
         ["network", "vpcs", "create", f"--name=test-{uuid.uuid1()}"]
     )
-    assert exit_code == 0
+    assert exit_code == 0, stderr
     assert jsonout["id"] is not None
     assert jsonout["status"] == "pending"
 
@@ -26,10 +26,10 @@ def test_network_vpcs_create():
 
 
 def test_network_vpcs_get():
-    exit_code, _, _, jsonout = run_cli(
+    exit_code, _, stderr, jsonout = run_cli(
         ["network", "vpcs", "get", network_test_context["vpc_id"]]
     )
-    assert exit_code == 0
+    assert exit_code == 0, stderr
     assert jsonout["id"] == network_test_context["vpc_id"]
 
     # Wait until VPC processing is over (hoping it will be)
@@ -39,20 +39,79 @@ def test_network_vpcs_get():
             ["network", "vpcs", "get", network_test_context["vpc_id"]]
         )
 
+
 def test_network_vpcs_list():
-    exit_code, _, _, jsonout = run_cli(["network", "vpcs", "list"])
-    assert exit_code == 0
+    exit_code, _, stderr, jsonout = run_cli(["network", "vpcs", "list"])
+    assert exit_code == 0, stderr
     assert len(jsonout["vpcs"]) > 0
 
 
 def test_network_ports_list():
-    exit_code, _, _, jsonout = run_cli(["network", "ports", "list"])
-    assert exit_code == 0
+    exit_code, _, stderr, jsonout = run_cli(["network", "ports", "list"])
+    assert exit_code == 0, stderr
     assert isinstance(jsonout, list)
 
 
+def test_network_natgateways_create():
+    exit_code, _, stderr, jsonout = run_cli(
+        [
+            "network",
+            "nat-gateways",
+            "create",
+            f"--name=test-{uuid.uuid1()}",
+            f"--vpc-id={network_test_context['vpc_id']}",
+            "--zone=br-se1-a",
+        ]
+    )
+
+    assert exit_code == 0, stderr
+    assert "id" in jsonout
+
+    network_test_context["natgateway_id"] = jsonout["id"]
+
+
+def test_network_natgateways_get():
+    exit_code, _, stderr, jsonout = run_cli(
+        [
+            "network",
+            "nat-gateways",
+            "get",
+            f"--nat-gateway-id={network_test_context['natgateway_id']}",
+        ]
+    )
+    assert exit_code == 0, stderr
+    assert "id" in jsonout
+    assert jsonout["id"] == network_test_context["natgateway_id"]
+
+
+def test_network_natgateways_list():
+    exit_code, _, stderr, jsonout = run_cli(
+        [
+            "network",
+            "nat-gateways",
+            "list",
+            f"--vpc-id={network_test_context['vpc_id']}",
+        ]
+    )
+    assert exit_code == 0, stderr
+    assert "result" in jsonout, jsonout
+
+
+def test_network_natgateway_delete():
+    exit_code, _, stderr, _ = run_cli(
+        [
+            "network",
+            "nat-gateways",
+            "delete",
+            network_test_context["natgateway_id"],
+            "--no-confirm",
+        ]
+    )
+    assert exit_code == 0, stderr
+
+
 def test_network_vpcs_delete():
-    exit_code, _, _, _ = run_cli(
+    exit_code, _, stderr, _ = run_cli(
         ["network", "vpcs", "delete", network_test_context["vpc_id"], "--no-confirm"]
     )
-    assert exit_code == 0
+    assert exit_code == 0, stderr
