54a1d442490e777c0194c17ad4aad2f7523d2412
Luiz Cavalcanti <luiz.cavalcanti@luizalabs.com>
2025-04-07 21:23:00 -0300

    chore: print stderr when config exit codes fail

--- tests/test_config.py
+++ tests/test_config.py
@@ -14,30 +14,30 @@ def test_config_list():
 
 
 def test_config_get_schema():
-    exit_code, _, _, jsonout = run_cli(["config", "get-schema", "env"])
-    assert exit_code == 0
+    exit_code, _, stderr, jsonout = run_cli(["config", "get-schema", "env"])
+    assert exit_code == 0, stderr
     assert jsonout["default"] == "prod"
 
-    exit_code, _, _, jsonout = run_cli(["config", "get-schema", "defaultOutput"])
-    assert exit_code == 0
+    exit_code, _, stderr, jsonout = run_cli(["config", "get-schema", "defaultOutput"])
+    assert exit_code == 0, stderr
     assert "default" not in jsonout
 
 
 def test_config_set():
-    exit_code, _, _, _ = run_cli(["config", "set", "workers", "999"])
-    assert exit_code == 0
+    exit_code, _, stderr, _ = run_cli(["config", "set", "workers", "999"])
+    assert exit_code == 0, stderr
 
 
 def test_config_get():
-    exit_code, _, _, jsonout = run_cli(["config", "get", "workers"])
-    assert exit_code == 0
+    exit_code, _, stderr, jsonout = run_cli(["config", "get", "workers"])
+    assert exit_code == 0, stderr
     assert jsonout == 999
 
 
 def test_config_delete():
-    exit_code, _, _, jsonout = run_cli(["config", "delete", "workers"])
-    assert exit_code == 0
+    exit_code, _, stderr, jsonout = run_cli(["config", "delete", "workers"])
+    assert exit_code == 0, stderr
 
-    exit_code, _, _, jsonout = run_cli(["config", "get", "workers"])
-    assert exit_code == 0
+    exit_code, _, stderr, jsonout = run_cli(["config", "get", "workers"])
+    assert exit_code == 0, stderr
     assert jsonout == {}
