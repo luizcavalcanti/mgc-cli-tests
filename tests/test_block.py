e2d37952d1bef2fb3f75d668778d55d9dacdb2b3
Luiz Cavalcanti <luiz.cavalcanti@luizalabs.com>
2025-04-15 10:51:42 -0300

    feat: improve block storage tests

--- tests/test_block.py
+++ tests/test_block.py
@@ -13,13 +13,13 @@ def _get_snapshot(snap_id):
 
 
 def test_bs_volume_types_list():
-    exit_code, _, _, jsonout = run_cli(["bs", "volume-types", "list"])
-    assert exit_code == 0
+    exit_code, _, stderr, jsonout = run_cli(["bs", "volume-types", "list"])
+    assert exit_code == 0, stderr
     assert len(jsonout["types"]) > 0
 
 
 def test_bs_volumes_create():
-    exit_code, _, _, jsonout = run_cli(
+    exit_code, _, stderr, jsonout = run_cli(
         [
             "bs",
             "volumes",
@@ -29,7 +29,7 @@ def test_bs_volumes_create():
             "--type.name=cloud_nvme10k",
         ]
     )
-    assert exit_code == 0
+    assert exit_code == 0, stderr
     assert "id" in jsonout
 
     block_test_context["volume_id"] = jsonout["id"]
@@ -42,8 +42,8 @@ def test_bs_volumes_create():
 
 
 def test_bs_volumes_get():
-    exit_code, _, _, jsonout = _get_volume(block_test_context["volume_id"])
-    assert exit_code == 0
+    exit_code, _, stderr, jsonout = _get_volume(block_test_context["volume_id"])
+    assert exit_code == 0, stderr
     assert "availability_zone" in jsonout
     assert "availability_zones" in jsonout
     assert "encrypted" in jsonout
@@ -66,7 +66,7 @@ def test_bs_snapshots_create():
             f"--volume.id={block_test_context["volume_id"]}",
         ]
     )
-    assert exit_code == 0, f"Command failed: {stderr}"
+    assert exit_code == 0, stderr
     assert "id" in jsonout
 
     block_test_context["snapshot_id"] = jsonout["id"]
@@ -79,16 +79,16 @@ def test_bs_snapshots_create():
 
 
 def test_bs_snapshots_list():
-    exit_code, _, _, jsonout = run_cli(["bs", "snapshots", "list"])
-    assert exit_code == 0
+    exit_code, _, stderr, jsonout = run_cli(["bs", "snapshots", "list"])
+    assert exit_code == 0, stderr
     assert len(jsonout["snapshots"]) > 0
 
 
 def test_bs_snapshots_get():
-    exit_code, _, _, jsonout = run_cli(
+    exit_code, _, stderr, jsonout = run_cli(
         ["bs", "snapshots", "get", block_test_context["snapshot_id"]]
     )
-    assert exit_code == 0
+    assert exit_code == 0, stderr
     assert "availability_zones" in jsonout
     assert "description" in jsonout
     assert "id" in jsonout
@@ -101,16 +101,16 @@ def test_bs_snapshots_get():
 
 
 def test_bs_snapshots_delete():
-    exit_code, _, _, jsonout = run_cli(
+    exit_code, _, stderr, jsonout = run_cli(
         ["bs", "snapshots", "delete", block_test_context["snapshot_id"], "--no-confirm"]
     )
-    assert exit_code == 0
+    assert exit_code == 0, stderr
     # Extra wait to relieve snapshot count for volume
     time.sleep(10)
 
 
 def test_bs_volumes_delete():
-    exit_code, _, _, jsonout = run_cli(
+    exit_code, _, stderr, jsonout = run_cli(
         ["bs", "volumes", "delete", block_test_context["volume_id"], "--no-confirm"]
     )
-    assert exit_code == 0
+    assert exit_code == 0, stderr
